13ae1e9a481104a7d76cab6d221562c4feca394f
Brando Rocha <86134682+Bran00@users.noreply.github.com>
2023-04-11 18:24:16 -0300

    fix: altere the next config

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => (
         <div className="mb-[50px] h-[2px] bg-white opacity-10" />
 
         <div className="flex items-center justify-between flex-wrap gap-4">
-          <h4 className="font-extrabold text-[24px] text-white">METAVERUS</h4>
+          <h4 className="font-extrabold text-[24px] text-white">METAVERSUS</h4>
           <p className="font-normal text-[14px] text-white opacity-50">
             Copyright © 2021 - 2022 Metaversus. All rights reserved.
           </p>
